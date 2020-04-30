8618fd693250c8f8f144f7faea838361e0a94a93
Samson Qian <samsonqian@gmail.com>
2020-04-30 15:33:24 -0700

    eval

--- src/modeling/evaluation.py
+++ src/modeling/evaluation.py
@@ -5,5 +5,16 @@ Hyperparameter tuning of models through processes like cross-validation.
 import numpy as np
 
 import tensorflow as tf
-from sklearn.model_selection import GridSearchCV, KFold
 import keras
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.metrics import make_scorer, accuracy_score
+
+
+def predict(mdl, test_X, test_y):
+    """
+
+    :param mdl:
+    :param test_X:
+    :param test_y:
+    :return:
+    """
