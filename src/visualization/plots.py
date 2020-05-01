815612f4ff5c79c20449ed899356e7c1aff20681
Samson Qian <samsonqian@gmail.com>
2020-05-01 00:31:06 -0700

    EDA

--- src/visualization/plots.py
+++ src/visualization/plots.py
@@ -48,16 +48,46 @@ def histogram(column, data, normal=True, kde=False, bins=None):
 
 def scatterplot(x, y, data, hue=None, regression=False):
     """
-    This function returns a seaborn barplot based on the data columns passed in.
+    This function returns a seaborn bar plot based on the data columns passed in.
+
     :param x: x-axis column as a string
     :param y: y-axis column as a string
     :param data: dataframe containing above columns
     :param hue: hue column as a string
     :param regression: boolean of whether to plot regression
-    :returns: scatterplot of the columns
+    :returns: scatter plot of the columns
     """
     if not regression:
         return sns.relplot(x=x, y=y, hue=hue, data=data)
     else:
         assert hue is None, "Can't have Hue with Regression Plot"
         return sns.regplot(x=x, y=y, data=data)
+
+
+def count_distribution(x, data, size=(14, 6)):
+    """
+    Plots and returns a count plot that displays the counts of each
+    category within a given column.
+
+    :param x: categorical column to display counts
+    :param data: dataframe containing columns
+    :param size: size of plot
+    :return: count plot of x
+    """
+    plt.figure(figsize=size)
+
+    g = sns.countplot(x=x, data=data)
+    g.set_title(x+" Distribution", fontsize=15)
+    g.set_xlabel(x, fontsize=13)
+    g.set_ylabel('Count', fontsize=13)
+
+    proportions = []
+    for status in g.patches:
+        height = status.get_height()
+        proportions.append(height)
+        g.text(status.get_x() + status.get_width() / 2.,
+               height + 3,
+               "{:1.2f}%".format(height / len(data) * 100),
+               ha="center", fontsize=12)
+
+    g.set_ylim(0, max(proportions) * 1.1)
